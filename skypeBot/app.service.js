3f1582fc155f6d75f8fe33ffa471d7b7d82f17dd
vitalics <vitalicset@yandex.ru>
2017-04-08 15:16:25 +0300

    Create subscribes methods

--- skypeBot/app.service.js
+++ skypeBot/app.service.js
@@ -2,7 +2,10 @@ var gitHubApi = require('../githubApi/common.service.js');
 
 class SkypeService {
     constructor(user) {
-
+        this.userNames = [];
+        this.tempUserNames = [];
+        this.subscribesRepos = [];
+        this.tempRepos = [];
         this.commonService = new gitHubApi.CommonService();
     }
     getUserRepos(user) {
@@ -11,6 +14,30 @@ class SkypeService {
         }).catch(err => {
             return err;
         })
+    }
+    getLastCommit(userName, repoName) {
+        this.commonService.getLastCommit(userName, repoName).then(repos => {
+            return repos;
+        }).catch(err => {
+            return err;
+        })
+    }
+
+    subscribeRepos(userName, repoName) {
+        this.subscribesRepos.push(repoName);
+        this.tempRepos.push(repoName);
+        this.userNames.push(userName);
+        this.tempUserNames.push(userName);
+    }
+
+    checkForUpdates() {
+
+    }
+
+    ping() {
+        setInterval({
+
+        }, 5000)
     }
 }
 
